896b9287d2188805612a1017bfc89f01d5189450
Aria Diniz <aria.diniz.dev@gmail.com>
2024-01-14 15:59:09 -0300

    Adding documentation to stack

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,6 +1,12 @@
 #include "stack.h"
 #include <stdlib.h>
 
+/*
+  Create a new Stack.
+
+  @return Return a pointer to the newly created Stack.
+  Don't forget to free memory after usage with deletestack(stack).
+*/
 struct Stack *createstack() {
   struct Stack *stack = malloc(sizeof(struct Stack));
   if (stack == NULL) {
@@ -10,6 +16,14 @@ struct Stack *createstack() {
   stack->top = NULL;
 }
 
+/*
+  Safely delete the Stack freeing the memory.
+  If you used this structure to store in the values pointers for
+  other manually allocated memory adresses, please free all pointers
+  before calling this method.
+  @param stack A pointer to a Stack.
+  @return Return 0 when successful. Return NULL if Stack is empty or NULL.
+*/
 void *deletestack(struct Stack *stack) {
   if (stack == NULL) {
     return NULL;
@@ -29,6 +43,13 @@ void *deletestack(struct Stack *stack) {
   return 0;
 }
 
+/*
+  Add a new element to the Stack.
+  @param stack A pointer to a Stack.
+  @param value A pointer to a value to be stored.
+  @return Return 0 if successful. Return NULL if Stack is NULL or empty,
+  or in case of error in memory allocation.
+*/
 void *addtostack(struct Stack *stack, void *value) {
   if (stack == NULL || value == NULL) {
     return NULL;
@@ -44,6 +65,13 @@ void *addtostack(struct Stack *stack, void *value) {
   return 0;
 }
 
+/*
+  Removes the top element of the Stack and return a pointer to it's value.
+  Don't forget to free this pointer afterward, if it point to a manually
+  allocated memory address.
+  @param stack A pointer to a Stack.
+  @return A pointer to the value stored. Return NULL if the Stack is empty.
+*/
 void *popfromstack(struct Stack *stack) {
   if (stack == NULL || stack->size == 0) {
     return NULL;
