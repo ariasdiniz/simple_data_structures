fdb12716fe13e08ef0078c5cdc239b4638a45a82
Aria Diniz <aria.diniz.dev@gmail.com>
2024-01-14 15:45:00 -0300

    implementing addtohash function.

--- src/hash/hash.c
+++ src/hash/hash.c
@@ -1,8 +1,6 @@
 #include "hash.h"
 #include <stdlib.h>
 
-#define CALL_HASHING_FUNCTION(a, b) a b
-
 HashTable *createhash() {
   HashTable *hash = malloc(sizeof(HashTable) * ARIA_DATA_STRUCTURES_HASH_MAX_SIZE);
   if (hash == NULL) {
@@ -23,4 +21,23 @@ HashTable *deletehash(HashTable *hash) {
   }
   free(hash);
   return 0;
+}
+
+unsigned int hashfunc(char *str) {
+    unsigned int hashValue = 0;
+    for (; *str != '\0'; str++) {
+        hashValue = 31 * hashValue + *str;
+    }
+    return hashValue % ARIA_DATA_STRUCTURES_HASH_MAX_SIZE;
+}
+
+void *addtohash(HashTable *hash, char *value) {
+  if (hash == NULL) {
+    return NULL;
+  }
+  LinkedList *temp = hash->bucket[hashfunc(value)];
+  unsigned int index = hashfunc(value);
+  hash->bucket[index] = temp == NULL ? createlist() : temp;
+  addtolist(hash->bucket[index], value);
+  return index;
 }
