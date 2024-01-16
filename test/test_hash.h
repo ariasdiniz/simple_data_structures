9cf4bb723da239cf934fab149f2001c0bbd4e46c
Aria Diniz <aria.diniz.dev@gmail.com>
2024-01-16 11:37:40 -0300

    including documentation for project

---
+++ test/test_hash.h
@@ -0,0 +1,57 @@
+#include "../src/hash/hash.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+HashTable *setup() {
+  return createhash();
+}
+
+void teardown(HashTable *hash) {
+  deletehash(hash);
+}
+
+void test_createhash() {
+  HashTable *hash = setup();
+  assert(hash != NULL);
+  teardown(hash);
+  printf("test_createhash passed\n");
+}
+
+void test_deletehash() {
+  HashTable *hash = setup();
+  assert(deletehash(hash) == 0);
+  assert(deletehash(NULL) == -1);
+
+  printf("test_deletehash passed\n");
+}
+
+void test_addtohash() {
+  HashTable *hash = setup();
+  assert(addtohash(hash, "key1", "value1") == 0);
+  assert(addtohash(NULL, "key2", "value2") == -1);
+  teardown(hash);
+  printf("test_addtohash passed\n");
+}
+
+void test_getfromhash() {
+  HashTable *hash = setup();
+  addtohash(hash, "key1", "value1");
+  char *value = getfromhash(hash, "key1");
+  assert(value != -1 && strcmp(value, "value1") == 0);
+  value = getfromhash(hash, "nonexistent");
+  assert(value == -1);
+  assert(getfromhash(NULL, "key1") == -1);
+  teardown(hash);
+  printf("test_getfromhash passed\n");
+}
+
+void runhashtests() {
+  printf("----------STARTING HASHTABLE TESTS----------\n");
+  test_createhash();
+  test_deletehash();
+  test_addtohash();
+  test_getfromhash();
+  printf("----------FINISHING HASHTABLE TESTS----------\n");
+}
